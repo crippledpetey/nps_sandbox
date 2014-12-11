72338859b9c209e332bb477da42f14586106568a
needplumbingsupplies-git <brandon@needplumbingsupplies.com>
2014-12-10 21:45:43 -0800

    outputting content of request to log

--- app/code/community/HALO/Haloebajessettings/Model/Observer.php
+++ app/code/community/HALO/Haloebajessettings/Model/Observer.php
@@ -8,7 +8,7 @@
 			//$observer contains data passed from when the event was triggered.
 	        //You can use this data to manipulate the order data before it's saved.
 	        //Uncomment the line below to log what is contained here:
-	        Mage::log($observer);
+	        Mage::log($_REQUEST);
 
 	        //Mage::log('After Admin Config Save:\n');
 		}
