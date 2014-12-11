6397929b452f1f23dcf074473023ec37673857f1
needplumbingsupplies-git <brandon@needplumbingsupplies.com>
2014-12-10 21:31:17 -0800

    Adding logging to the observer call
    log contains dump of the request

--- app/code/community/HALO/Haloebajessettings/Model/Observer.php
+++ app/code/community/HALO/Haloebajessettings/Model/Observer.php
@@ -4,6 +4,13 @@
 
 		public function afterConfigSave(){
 			/* RUNS AFTER ADMIN CONFIGURATION IS SAVED */
+
+			//$observer contains data passed from when the event was triggered.
+	        //You can use this data to manipulate the order data before it's saved.
+	        //Uncomment the line below to log what is contained here:
+	        Mage::log($observer);
+
+	        //Mage::log('After Admin Config Save:\n');
 		}
 	}
 ?>
