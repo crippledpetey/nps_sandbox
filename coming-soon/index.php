4548fcf79e67329a204dc98a69d4bb09ca7efd01
Brandon Thomas <development@needplumbingsupplies.com>
2015-02-10 12:54:25 -0800

    htaccess12

--- coming-soon/index.php
+++ coming-soon/index.php
@@ -4,84 +4,18 @@
     <meta name="description" content="">
     <meta name="keywords" content="">
     <meta name="robots" content="NOINDEX,NOFOLLOW">
-    <link rel="icon" href="/favicon.ico" type="image/x-icon">
-    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="/coming-soon/favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="/coming-soon/favicon.ico" type="image/x-icon">
     <link href="http://fonts.googleapis.com/css?family=Roboto:400,700" rel="stylesheet" type="text/css">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <meta name="apple-mobile-web-app-capable" content="yes">
-    <link rel="stylesheet" type="text/css" href="/styles.css" media="all">
+    <link rel="stylesheet" type="text/css" href="/coming-soon/styles.css" media="all">
     <!--[if lt IE 8]>
     <link rel="stylesheet" type="text/css" href="http://sandbox.needplumbingsupplies.com/skin/frontend/ebajes/default/css/styles-ie.css" media="all" />
     <![endif]-->
 </head>
 <div class="header-container">
-    <div class="quick-access">
-        <div class="inner-quick-access">
-            <div id="header-logo-container">
-                <h1 class="logo"><strong>Need Plumbing Supplies</strong><a href="http://sandbox.needplumbingsupplies.com/" title="Need Plumbing Supplies" class="logo"><img src="http://sandbox.needplumbingsupplies.com/skin/frontend/ebajes/default/images/logo.png" alt="Need Plumbing Supplies"></a></h1>
-            </div>
-            <div class="header-container-left" style="margin-left: 30px;">
-                <p class="welcome-msg">Welcome to Need Plumbing Supplies </p>
-                <a href="https://sandbox.needplumbingsupplies.com/customer/account/login/">Login</a>
-                <span>or</span>
-                <a href="https://sandbox.needplumbingsupplies.com/customer/account/create/">Signup</a>
-            </div>
-            <ul class="links">
-                <li class="first"><a href="https://sandbox.needplumbingsupplies.com/customer/account/" title="My Account">My Account</a></li>
-                <li><a href="https://sandbox.needplumbingsupplies.com/wishlist/" title="My Wishlist">My Wishlist</a></li>
-                <li><a href="http://sandbox.needplumbingsupplies.com/checkout/cart/" title="Shopping Cart" class="top-link-cart">Shopping Cart</a></li>
-                <li class=" last"><a href="https://sandbox.needplumbingsupplies.com/checkout/" title="Checkout" class="top-link-checkout">Checkout</a></li>
-            </ul>
-            <div id="header-phone">Call Us: <a href="tel:800-555-5555" title="Call Us!">800-555-5555</a></div>                                  <div class="div_link-cart header-top-cart">
-            <div class="div_link-cart_inner">
-                <!--div class="top-cart">
-            </div-->
-            <!--div class="top-checkout">
-        </div-->
-        <div id="top_cart_sidebar" style="display: none;">
-            <div class="block block-cart">
-                <div class="block-title">
-                    <strong><span>Shopping Cart</span></strong>
-                </div>
-                <div class="block-content">
-                    <p class="empty">You have no items in your shopping cart.</p>
-                </div>
-            </div>
-        </div>
-    </div>
+
 </div>
-</div>
-</div>
-<div class="header-bottom">
-<div class="top-search">
-<form id="search_mini_form" action="http://sandbox.needplumbingsupplies.com/catalogsearch/result/" method="get">
-    <div class="form-search">
-        <!--<label for="search">Search:</label>-->
-        <input id="search" type="text" name="q" value="" class="input-text" maxlength="128" autocomplete="off">
-        <span id="clear-search">X</span>
-        <div id="search-buttons">
-            <button type="submit" title="Search" class="search-button">
-            <span>
-            <span>Search</span>
-            </span>
-            </button>
-        </div>
-        <a id="advanced-search-link" href="http://sandbox.needplumbingsupplies.com/catalogsearch/advanced/">Advanced Search</a>
-        <div id="search_autocomplete" class="search-autocomplete" style="display: none;"></div>
-        <script type="text/javascript">
-        //<![CDATA[
-        var searchForm = new Varien.searchForm('search_mini_form', 'search', 'Search entire store here...');
-        searchForm.initAutocomplete('http://sandbox.needplumbingsupplies.com/catalogsearch/ajax/suggest/', 'search_autocomplete');
-        //]]>
-        </script>
-    </div>
-</form>
-</div>
-<div class="clearer"></div>
-<div class="nav-container">
-<ul id="nav">
-</ul>
-</div>
-</div>
-<div id="mobile-menu-toggle" onclick="toggleMobileMenu()"> ☰</div>
+<div id="primary">
 </div>
