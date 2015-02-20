64430151d5f63159073ee16125b8c2940b7a793d
Brandon Thomas <development@needplumbingsupplies.com>
2015-02-19 16:24:09 -0800

    WYSIWYG
    Extending tinymce settings

--- js/mage/adminhtml/wysiwyg/tiny_mce/setup.js
+++ js/mage/adminhtml/wysiwyg/tiny_mce/setup.js
@@ -103,6 +103,7 @@ tinyMceWysiwygSetup.prototype =
             theme_advanced_buttons2 : 'cut,copy,paste,pastetext,pasteword,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,cleanup,help,code,|,forecolor,backcolor',
             theme_advanced_buttons3 : 'tablecontrols,|,hr,removeformat,visualaid,|,sub,sup,|,charmap,iespell,media,advhr,|,ltr,rtl,|,fullscreen',
             theme_advanced_buttons4 : 'insertlayer,moveforward,movebackward,absolute,|,styleprops,|,cite,abbr,acronym,del,ins,attribs,|,visualchars,nonbreaking,pagebreak',
+            extended_valid_elements : 'dd,dt',
             theme_advanced_toolbar_location : 'top',
             theme_advanced_toolbar_align : 'left',
             theme_advanced_statusbar_location : 'bottom',
