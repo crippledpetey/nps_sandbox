4e18d04dde46a7ffc30fc481995f6a09a037abba
Brandon Thomas <development@needplumbingsupplies.com>
2015-03-03 12:27:34 -0800

    Added Attribute Set to Category Product

--- app/code/core/Mage/Adminhtml/Block/Catalog/Category/Tab/Product.php
+++ app/code/core/Mage/Adminhtml/Block/Catalog/Category/Tab/Product.php
@@ -31,132 +31,134 @@
  * @package    Mage_Adminhtml
  * @author      Magento Core Team <core@magentocommerce.com>
  */
-class Mage_Adminhtml_Block_Catalog_Category_Tab_Product extends Mage_Adminhtml_Block_Widget_Grid
-{
+class Mage_Adminhtml_Block_Catalog_Category_Tab_Product extends Mage_Adminhtml_Block_Widget_Grid {
 
-    public function __construct()
-    {
-        parent::__construct();
-        $this->setId('catalog_category_products');
-        $this->setDefaultSort('entity_id');
-        $this->setUseAjax(true);
-    }
+	public function __construct() {
+		parent::__construct();
+		$this->setId('catalog_category_products');
+		$this->setDefaultSort('entity_id');
+		$this->setUseAjax(true);
+	}
 
-    public function getCategory()
-    {
-        return Mage::registry('category');
-    }
+	public function getCategory() {
+		return Mage::registry('category');
+	}
 
-    protected function _addColumnFilterToCollection($column)
-    {
-        // Set custom filter for in category flag
-        if ($column->getId() == 'in_category') {
-            $productIds = $this->_getSelectedProducts();
-            if (empty($productIds)) {
-                $productIds = 0;
-            }
-            if ($column->getFilter()->getValue()) {
-                $this->getCollection()->addFieldToFilter('entity_id', array('in'=>$productIds));
-            }
-            elseif(!empty($productIds)) {
-                $this->getCollection()->addFieldToFilter('entity_id', array('nin'=>$productIds));
-            }
-        }
-        else {
-            parent::_addColumnFilterToCollection($column);
-        }
-        return $this;
-    }
+	protected function _addColumnFilterToCollection($column) {
+		// Set custom filter for in category flag
+		if ($column->getId() == 'in_category') {
+			$productIds = $this->_getSelectedProducts();
+			if (empty($productIds)) {
+				$productIds = 0;
+			}
+			if ($column->getFilter()->getValue()) {
+				$this->getCollection()->addFieldToFilter('entity_id', array('in' => $productIds));
+			} elseif (!empty($productIds)) {
+				$this->getCollection()->addFieldToFilter('entity_id', array('nin' => $productIds));
+			}
+		} else {
+			parent::_addColumnFilterToCollection($column);
+		}
+		return $this;
+	}
 
-    protected function _prepareCollection()
-    {
-        if ($this->getCategory()->getId()) {
-            $this->setDefaultFilter(array('in_category'=>1));
-        }
-        $collection = Mage::getModel('catalog/product')->getCollection()
-            ->addAttributeToSelect('name')
-            ->addAttributeToSelect('sku')
-            ->addAttributeToSelect('price')
-            ->addStoreFilter($this->getRequest()->getParam('store'))
-            ->joinField('position',
-                'catalog/category_product',
-                'position',
-                'product_id=entity_id',
-                'category_id='.(int) $this->getRequest()->getParam('id', 0),
-                'left');
-        $this->setCollection($collection);
+	protected function _prepareCollection() {
+		if ($this->getCategory()->getId()) {
+			$this->setDefaultFilter(array('in_category' => 1));
+		}
+		$collection = Mage::getModel('catalog/product')->getCollection()
+		                                               ->addAttributeToSelect('name')
+		                                               ->addAttributeToSelect('sku')
+		                                               ->addAttributeToSelect('price')
+		                                               ->addStoreFilter($this->getRequest()->getParam('store'))
+		                                               ->joinField('position',
+			                                               'catalog/category_product',
+			                                               'position',
+			                                               'product_id=entity_id',
+			                                               'category_id=' . (int) $this->getRequest()->getParam('id', 0),
+			                                               'left');
+		$this->setCollection($collection);
 
-        if ($this->getCategory()->getProductsReadonly()) {
-            $productIds = $this->_getSelectedProducts();
-            if (empty($productIds)) {
-                $productIds = 0;
-            }
-            $this->getCollection()->addFieldToFilter('entity_id', array('in'=>$productIds));
-        }
+		if ($this->getCategory()->getProductsReadonly()) {
+			$productIds = $this->_getSelectedProducts();
+			if (empty($productIds)) {
+				$productIds = 0;
+			}
+			$this->getCollection()->addFieldToFilter('entity_id', array('in' => $productIds));
+		}
 
-        return parent::_prepareCollection();
-    }
+		return parent::_prepareCollection();
+	}
 
-    protected function _prepareColumns()
-    {
-        if (!$this->getCategory()->getProductsReadonly()) {
-            $this->addColumn('in_category', array(
-                'header_css_class' => 'a-center',
-                'type'      => 'checkbox',
-                'name'      => 'in_category',
-                'values'    => $this->_getSelectedProducts(),
-                'align'     => 'center',
-                'index'     => 'entity_id'
-            ));
-        }
-        $this->addColumn('entity_id', array(
-            'header'    => Mage::helper('catalog')->__('ID'),
-            'sortable'  => true,
-            'width'     => '60',
-            'index'     => 'entity_id'
-        ));
-        $this->addColumn('name', array(
-            'header'    => Mage::helper('catalog')->__('Name'),
-            'index'     => 'name'
-        ));
-        $this->addColumn('sku', array(
-            'header'    => Mage::helper('catalog')->__('SKU'),
-            'width'     => '80',
-            'index'     => 'sku'
-        ));
-        $this->addColumn('price', array(
-            'header'    => Mage::helper('catalog')->__('Price'),
-            'type'  => 'currency',
-            'width'     => '1',
-            'currency_code' => (string) Mage::getStoreConfig(Mage_Directory_Model_Currency::XML_PATH_CURRENCY_BASE),
-            'index'     => 'price'
-        ));
-        $this->addColumn('position', array(
-            'header'    => Mage::helper('catalog')->__('Position'),
-            'width'     => '1',
-            'type'      => 'number',
-            'index'     => 'position',
-            'editable'  => !$this->getCategory()->getProductsReadonly()
-            //'renderer'  => 'adminhtml/widget_grid_column_renderer_input'
-        ));
+	protected function _prepareColumns() {
+		$attr_set_options = Mage::getResourceModel('eav/entity_attribute_set_collection')
+			->setEntityTypeFilter(Mage::getModel('catalog/product')->getResource()->getTypeId())
+			->load()
+			->toOptionHash();
 
-        return parent::_prepareColumns();
-    }
+		if (!$this->getCategory()->getProductsReadonly()) {
+			$this->addColumn('in_category', array(
+				'header_css_class' => 'a-center',
+				'type' => 'checkbox',
+				'name' => 'in_category',
+				'values' => $this->_getSelectedProducts(),
+				'align' => 'center',
+				'index' => 'entity_id',
+			));
+		}
+		$this->addColumn('entity_id', array(
+			'header' => Mage::helper('catalog')->__('ID'),
+			'sortable' => true,
+			'width' => '60',
+			'index' => 'entity_id',
+		));
+		$this->addColumn('name', array(
+			'header' => Mage::helper('catalog')->__('Name'),
+			'index' => 'name',
+		));
+		$this->addColumn('sku', array(
+			'header' => Mage::helper('catalog')->__('SKU'),
+			'width' => '80',
+			'index' => 'sku',
+		));
+		$this->addColumn('price', array(
+			'header' => Mage::helper('catalog')->__('Price'),
+			'type' => 'currency',
+			'width' => '1',
+			'currency_code' => (string) Mage::getStoreConfig(Mage_Directory_Model_Currency::XML_PATH_CURRENCY_BASE),
+			'index' => 'price',
+		));
+		$this->addColumn('set_name', array(
+			'header' => Mage::helper('catalog')->__('Attrib. Set Name'),
+			'width' => '1',
+			'type' => 'options',
+			'index' => 'attribute_set_id',
+			'options' => $attr_set_options,
+			//'renderer'  => 'adminhtml/widget_grid_column_renderer_input'
+		));
+		$this->addColumn('position', array(
+			'header' => Mage::helper('catalog')->__('Position'),
+			'width' => '1',
+			'type' => 'number',
+			'index' => 'position',
+			'editable' => !$this->getCategory()->getProductsReadonly(),
+			//'renderer'  => 'adminhtml/widget_grid_column_renderer_input'
+		));
 
-    public function getGridUrl()
-    {
-        return $this->getUrl('*/*/grid', array('_current'=>true));
-    }
+		return parent::_prepareColumns();
+	}
 
-    protected function _getSelectedProducts()
-    {
-        $products = $this->getRequest()->getPost('selected_products');
-        if (is_null($products)) {
-            $products = $this->getCategory()->getProductsPosition();
-            return array_keys($products);
-        }
-        return $products;
-    }
+	public function getGridUrl() {
+		return $this->getUrl('*/*/grid', array('_current' => true));
+	}
+
+	protected function _getSelectedProducts() {
+		$products = $this->getRequest()->getPost('selected_products');
+		if (is_null($products)) {
+			$products = $this->getCategory()->getProductsPosition();
+			return array_keys($products);
+		}
+		return $products;
+	}
 
 }
-
