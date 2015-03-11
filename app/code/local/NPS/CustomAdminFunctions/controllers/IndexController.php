5c22495f7f235d1be2391940ee86160a6dc897c4
Brandon Thomas <development@needplumbingsupplies.com>
2015-03-10 21:42:47 -0700

    Custom Attribute Styling
    More Styling and added a new section with some dummy controls for now

--- app/code/local/NPS/CustomAdminFunctions/controllers/IndexController.php
+++ app/code/local/NPS/CustomAdminFunctions/controllers/IndexController.php
@@ -8,19 +8,20 @@ class NPS_CustomAdminFunctions_IndexController extends Mage_Adminhtml_Controller
 		$this->requestFunctions();
 
 		//check for bthom function controlling the page output for primary content
-		$btf = 0; //default welcome page
+		$this->btf = 0; //default welcome page
 		if (isset($_GET['btf'])) {
-			$btf = $_GET['btf'];
+			$this->btf = $_GET['btf'];
 		}
 
 		//function array to control output of primary content
 		$displayModes = array(
 			'npsWelcomePage',
 			'createAddOptionsAttributeForm',
+			'customAttributeOptions',
 		);
 
 		$primaryContent = '<style>' . file_get_contents(Mage::getBaseDir('base') . DIRECTORY_SEPARATOR . 'app' . DIRECTORY_SEPARATOR . 'code' . DIRECTORY_SEPARATOR . 'local' . DIRECTORY_SEPARATOR . 'NPS' . DIRECTORY_SEPARATOR . 'CustomAdminFunctions' . DIRECTORY_SEPARATOR . 'Helper' . DIRECTORY_SEPARATOR . 'adminStyle.css') . '</style>';
-		$primaryContent .= '<div id="nps-custom-attr-manager-container">' . call_user_func(array($this, $displayModes[$btf])) . '</div>';
+		$primaryContent .= '<div id="nps-custom-attr-manager-container">' . call_user_func(array($this, $displayModes[$this->btf])) . '</div>';
 
 		//load the layout
 		$this->loadLayout();
@@ -45,16 +46,21 @@ class NPS_CustomAdminFunctions_IndexController extends Mage_Adminhtml_Controller
 		//render the layout
 		$this->renderLayout();
 	}
+/**
+PAGE LOAD FUNCTIONS THAT CONTROL UPDATES
+ */
 
 	public function requestFunctions() {
 
 		//check if form is subitted
 		if (isset($_POST['nps_function'])) {
+			$refresh = false;
 			$append_url = null;
 
 			//if mass attribute addition
 			if ($_POST['nps_function'] == 'mass_attr_option_add') {
 
+				//append the btf number to the url
 				$append_url = 'btf=1';
 
 				//set the attribute name
@@ -72,12 +78,28 @@ class NPS_CustomAdminFunctions_IndexController extends Mage_Adminhtml_Controller
 
 				//process new options
 				$this->addAttributeOptions($attr_code, $attr_options, $sort_start);
+
+				//trigger page refresh
+				$refresh = true;
+			} elseif ($_POST['nps_function'] == 'nps_attr_option_settings') {
+
+				//set the refresh url
+				$append_url = 'btf=2&attr=' . $_POST['attribute_id'];
+
+				//trigger page refresh
+				$refresh = true;
 			}
 
-			Mage::app()->getFrontController()->getResponse()->setRedirect($_SERVER['REQUEST_URI'] . '?' . $append_url);
+			//if refresh is true then reload the page to prevent duplicate posting
+			if ($refresh) {
+				Mage::app()->getFrontController()->getResponse()->setRedirect($_SERVER['REQUEST_URI'] . '?' . $append_url);
+			}
 		}
 	}
 
+/**
+HTML OUTPUT MEHTODS
+ */
 	private function leftColumnHtml() {
 
 		//set url parts
@@ -93,8 +115,11 @@ class NPS_CustomAdminFunctions_IndexController extends Mage_Adminhtml_Controller
 		$html = '<h2 style="border-bottom: 1px dotted #d9d9d9;font-size:15px;">NPS Custom Attribute Tools</h2>';
 		$html .= '<ul id="nps-admin-custom-attr-nav">';
 
+		//attribute controls
+		$html .= '<a href="' . $url_base . '?btf=2" title="Attribute Controls"><li class="' . $this->active(2, $this->btf) . '">Attribute Custom Controls</li></a>';
+
 		//mass add options
-		$html .= '<a href="' . $url_base . '?btf=1" title="Mass Attribute Option Addition"><li data-content-body="">Mass Attribute Option Addition</li></a>';
+		$html .= '<a href="' . $url_base . '?btf=1" title="Mass Attribute Option Addition"><li class="' . $this->active(1, $this->btf) . '">Mass Attribute Option Addition</li></a>';
 
 		//close the list
 		$html .= '</ul>';
@@ -156,7 +181,7 @@ class NPS_CustomAdminFunctions_IndexController extends Mage_Adminhtml_Controller
 		$html .= '<textarea id="nps_attr_new_options" name="nps_attr_new_options" class="full-width" required></textarea><br>';
 
 		//submit button
-		$html .= '<input type="submit" value="Update Attribute" style="border-width: 1px;border-style: solid;border-color: #ed6502 #a04300 #a04300 #ed6502;padding: 1px 7px 2px 7px;background: #ffac47 url(images/btn_bg.gif) repeat-x 0 100%;color: #fff;font: bold 12px arial, helvetica, sans-serif;cursor: pointer;text-align: center !important;white-space: nowrap;">';
+		$html .= '<input type="submit" value="Update Attribute">';
 
 		//close form
 		$html .= '</form>';
@@ -164,18 +189,119 @@ class NPS_CustomAdminFunctions_IndexController extends Mage_Adminhtml_Controller
 		return $html;
 	}
 
-	public function addAttributeOptions($attribute_code, array $optionsArray, $sort_start) {
+	private function customAttributeOptions() {
+		//start boody
+		$html = '<h1>Custom Attribute Options</h1>';
+
+		//check for attribute being set
+		if (!isset($_GET['attr'])) {
+
+			$html .= '<form id="nps_attr_option_select_attr" name="nps_attr_option_select_attr" method="get" action="' . $_SERVER['PHP_SELF'] . '" enctype="multipart/form-data">';
+			$html .= '<input type="hidden" name="btf" value="2">';
+
+			$html .= '<label for="nps_attr_select">Select Attribute</label>';
+			$html .= '<select name="attr" required value=""><option>SELECT ATTRIBUTE</option>';
+
+			//get the list of attribute that can have options selected
+			$attributes = Mage::getResourceModel('catalog/product_attribute_collection')->getItems();
+			foreach ($attributes as $attribute) {
+				if ($attribute->getFrontendLabel() !== '' && !empty($attribute->getFrontendLabel())) {
+					$html .= '<option value="' . $attribute->getAttributecode() . '">' . $attribute->getFrontendLabel() . '</option>';
+				}
+			}
+
+			//close select box
+			$html .= '</select><div class="clearer big"></div>';
+
+			$submit_buttom = 'Configure Attribute';
+
+		} else {
+			$attribute_id = $_GET['attr'];
+
+			//start form & include hidden form key and function command
+			$html .= '<form id="nps_attr_options" name="nps_attr_options" method="post" action="' . $_SERVER['PHP_SELF'] . '" enctype="multipart/form-data">';
+			$html .= '<input type="hidden" name="nps_function" value="nps_attr_option_settings">';
+			$html .= '<input type="hidden" name="form_key" value="' . Mage::getSingleton('core/session')->getFormKey() . '">';
+			$html .= '<input type="hidden" name="attribute_id" value="' . $attribute_id . '">';
+
+			//show the attribute ID
+			$html .= '<div class="half-block">';
+			$html .= '<label for="nps_attr_select">Attribute Code</label>';
+			$html .= '<input type="text" disabled name="attribute_code_display" value="' . $attribute_id . '">';
+			$html .= '</div>';
+
+			if ($attributeRaw = Mage::getSingleton("eav/config")->getAttribute('catalog_product', $attribute_id, 'attribute_id')) {
+				//var_dump($attributeRaw);
+				$html .= '<div class="half-block">';
+				$html .= '<label for="nps_attr_select">Attribute ID</label>';
+				$html .= '<input type="text" disabled name="attribute_id_display" value="' . $attributeRaw->getId() . '">';
+				$html .= '</div>';
+
+				$html .= '<div class="clearer small"></div>';
+
+				//carry child up to container products
+				$html .= '<div class="half-block">';
+				$html .= '<label for="attr_option_carry_parent">Carry Over to Parent Container Product</label>';
+				$html .= '<input type="checkbox" name="attr_option_carry_parent" checked><br>';
+				$html .= '</div>';
+
+				//how to handle duplicates
+				$html .= '<div class="half-block">';
+				$html .= '<label for="attr_option_duplicate_handling">Duplicate Handling</label>';
+				$html .= '<select name="attr_option_duplicate_handling"><option value=""></option>';
+				$html .= '<option value="override">Override (newest products value is used)</option>';
+				$html .= '<option value="append">Override (creates a comma separated list)</option>';
+				$html .= '<option value="hide">Override (Hides all values if they differ)</option>';
+				$html .= '</select>';
+				$html .= '</div>';
+
+				$html .= '<div class="clearer small"></div>';
+
+				//add to product description section
+				$html .= '<div class="half-block">';
+				$html .= '<label for="attr_option_add_prd_desc">Add to Product Description Section</label>';
+				$html .= '<input type="checkbox" name="attr_option_add_prd_desc" checked>';
+				$html .= '</div>';
+
+				$html .= '<div class="half-block">';
+				$html .= '<label for="attr_option_desc_location">Description Location</label>';
+				$html .= '<select name="attr_option_desc_location"><option value=""></option>';
+				$html .= '<option value="techspecs">Technical Specifications</option>';
+				$html .= '<option value="features">Product Features</option>';
+				$html .= '<option value="manufacturer_info">Manufacturer Specific Features</option>';
+				$html .= '</select>';
+				$html .= '</div>';
+
+			} else {
+				//cant find the attribute
+				$html .= '<h2 style="color: red; text-transform:uppercase;">we\'re sorry but there was an error. please go back and try again by selecting another attirbute.</h2>';
+			}
+
+			$html .= '<div class="clearer big"></div>';
+
+			//submit button value
+			$submit_buttom = 'Save Attribute';
+		}
+
+		//submit button
+		$html .= '<input type="submit" value="' . $submit_buttom . '">';
+
+		//close form
+		$html .= '</form>';
+
+		return $html;
+	}
+
+/**
+DATABASE AND OTHER UPDATE METHODS CALLED BY  $this->requestFunctions()
+ */
+
+	protected function addAttributeOptions($attribute_code, array $optionsArray, $sort_start) {
 
 		//database read adapter
-		$sqlread = Mage::getSingleton('core/resource')->getConnection('core_read');
-		$sqlwrite = Mage::getSingleton('core/resource')->getConnection('core_write');
-		$tablePrefix = (string) Mage::getConfig()->getTablePrefix();
+		$this->setConnection();
 
-		/*
-		$tableOptions = Mage::getSingleton('core/resource')->getTable('eav_attribute_option');
-		$tableOptionValues = Mage::getSingleton('core/resource')->getTable('eav_attribute_option_value');
-		 */
-
+		//make sure we can find the attribute
 		if ($attributeRaw = Mage::getSingleton("eav/config")->getAttribute('catalog_product', $attribute_code, 'attribute_id')) {
 
 			//get and clean existing options
@@ -202,19 +328,65 @@ class NPS_CustomAdminFunctions_IndexController extends Mage_Adminhtml_Controller
 					'attribute_id' => $attributeId,
 					'sort_order' => $sortOrder + $sort_start,
 				);
-				$sqlwrite->insert('eav_attribute_option', $data);
+				$this->sqlwrite->insert('eav_attribute_option', $data);
 
 				// add option label
-				$optionId = (int) $sqlread->lastInsertId('eav_attribute_option', 'option_id');
+				$optionId = (int) $this->sqlread->lastInsertId('eav_attribute_option', 'option_id');
 				$data = array(
 					'option_id' => $optionId,
 					'store_id' => 0,
 					'value' => $label,
 				);
-				$sqlwrite->insert('eav_attribute_option_value', $data);
+				$this->sqlwrite->insert('eav_attribute_option_value', $data);
 			}
 		}
+	}
 
+/**
+INFASTRUCTURE METHODS
+ */
+
+	private function setConnection() {
+
+		//database read adapter
+		$this->sqlread = Mage::getSingleton('core/resource')->getConnection('core_read');
+		$this->sqlwrite = Mage::getSingleton('core/resource')->getConnection('core_write');
+		//database table prefix
+		$this->tablePrefix = (string) Mage::getConfig()->getTablePrefix();
+	}
+
+	public function checked($value, $test, $noOutput = false) {
+		if ($value == $test) {
+			if ($noOutput) {
+				return true;
+			} else {
+				return ' checked ';
+			}
+		} else {
+			return false;
+		}
+	}
+	public function selected($value, $test, $noOutput = false) {
+		if ($value == $test) {
+			if ($noOutput) {
+				return true;
+			} else {
+				return ' selected ';
+			}
+		} else {
+			return false;
+		}
+	}
+	public function active($value, $test, $noOutput = false) {
+		if ($value == $test) {
+			if ($noOutput) {
+				return true;
+			} else {
+				return ' active ';
+			}
+		} else {
+			return false;
+		}
 	}
 
 }
