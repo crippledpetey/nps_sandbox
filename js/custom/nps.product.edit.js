c048d9d37a9f7e73296a18158be00d0c6171b803
Brandon Thomas <development@needplumbingsupplies.com>
2015-03-27 18:53:48 -0700

    Product Edit Helper
    Added JS to the product edit page that makes the name input box red when
    the title is too long

--- js/custom/nps.product.edit.js
+++ js/custom/nps.product.edit.js
@@ -3,5 +3,15 @@ jQuery(document).ready(function($){
 		$(this).addClass("helloClassname");
 		$(this).prependTo($(this).parent("select"));
 	});
+	console.log("ghello there");
+
+	$("input#name").keyup(function(){
+		console.log( $(this).val().length );
+		if($(this).val().length >= 134 ){
+			$(this).css({"box-shadow":"0 0 10px red"});
+		} else {
+			$(this).css({"box-shadow":"none"});
+		}
+	});
 });
 
