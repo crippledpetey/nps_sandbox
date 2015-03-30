fe0b194fea2e30e5e0c9b3b7ebe9cd6b805acb63
Brandon Thomas <development@needplumbingsupplies.com>
2015-03-30 11:03:22 -0700

    Category SEO
    Uploading the Category SEO
    Enables tab on the edit category page that adds functionality to do
    CANONICAL / REDIRECT and other major controls.

--- js/custom/nps.product.edit.js
+++ js/custom/nps.product.edit.js
@@ -1,10 +1,9 @@
 jQuery(document).ready(function($){
-
 	function checkNameLength(){
 		var manuLen = $("#manufacturer option:selected").text().length;
 		var skuLen = $("#sku").val().length;
 		var nameLen = $("input#name").val().length;
-		
+
 		if( ( manuLen + skuLen + nameLen ) >= 134 ){
 			$("input#name").css({"box-shadow":"0 0 10px red"});
 		} else {
@@ -15,7 +14,6 @@ jQuery(document).ready(function($){
 		$(this).addClass("helloClassname");
 		$(this).prependTo($(this).parent("select"));
 	});
-
 	//make sure product name doesn't run into the price on product drop pages
 	checkNameLength();
 	$("input#name").keyup(function(){
