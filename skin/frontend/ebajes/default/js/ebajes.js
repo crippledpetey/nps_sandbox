57a80b2bd96abe3f2bd7feb33cbba7d4b7608b52
Brandon Thomas <development@needplumbingsupplies.com>
2015-04-07 12:37:34 -0700

    NPS Auto Insert on Copy
    Changed it so locally it wouldn't appear

--- skin/frontend/ebajes/default/js/ebajes.js
+++ skin/frontend/ebajes/default/js/ebajes.js
@@ -188,35 +188,39 @@ jQuery(document).ready(function($){
         }  
     });
 
-    //adds content to anything that someone copies
-    $("body").bind('copy', function (e) {
-        if (typeof window.getSelection == "undefined") return; //IE8 or earlier...
+    //adds content to anything that someone copies if they are not flagged as exceptions
+    if( $.inArray($("#user-ip").data('uip'), ["50.255.234.190", "50.255.234.189", "50.255.234.188", "50.255.234.187", "50.255.234.186", "50.255.234.185", "127.0.0.1"] ) < 0 ){
+        $("body").bind('copy', function (e) {
+            if (typeof window.getSelection == "undefined") return; //IE8 or earlier...
 
-        var body_element = document.getElementsByTagName('body')[0];
-        var selection = window.getSelection();
+            var body_element = document.getElementsByTagName('body')[0];
+            var selection = window.getSelection();
 
-        //if the selection is short let's not annoy our users
-        if (("" + selection).length < 30) return;
+            //if the selection is short let's not annoy our users
+            if (("" + selection).length < 30) return;
 
-        //create a div outside of the visible area
-        var newdiv = document.createElement('div');
-        newdiv.style.position = 'absolute';
-        newdiv.style.left = '-99999px';
-        body_element.appendChild(newdiv);
-        newdiv.appendChild(selection.getRangeAt(0).cloneContents());
+            //create a div outside of the visible area
+            var newdiv = document.createElement('div');
+            newdiv.style.position = 'absolute';
+            newdiv.style.left = '-99999px';
+            body_element.appendChild(newdiv);
+            newdiv.appendChild(selection.getRangeAt(0).cloneContents());
 
-        //we need a <pre> tag workaround
-        //otherwise the text inside "pre" loses all the line breaks!
-        if (selection.getRangeAt(0).commonAncestorContainer.nodeName == "PRE") {
-            newdiv.innerHTML = "<pre>" + newdiv.innerHTML + "</pre>";
-        }
+            //we need a <pre> tag workaround
+            //otherwise the text inside "pre" loses all the line breaks!
+            if (selection.getRangeAt(0).commonAncestorContainer.nodeName == "PRE") {
+                newdiv.innerHTML = "<pre>" + newdiv.innerHTML + "</pre>";
+            }
 
-        newdiv.innerHTML += "<br />Find all the best home products at &copy; <a href='"
-            + document.location.href + "'>NeedPlumbingSupplies.com</a>";
+            newdiv.innerHTML += "<br />Find all the best home products at &copy; <a href='"
+                + document.location.href + "'>NeedPlumbingSupplies.com</a>";
 
-        selection.selectAllChildren(newdiv);
-        window.setTimeout(function () { body_element.removeChild(newdiv); }, 200);
-    });
+            selection.selectAllChildren(newdiv);
+            window.setTimeout(function () { body_element.removeChild(newdiv); }, 200);
+        });
+    } else {
+        console.log( "Internal User: "+$("#user-ip").data('uip') );
+    }
     
     //GET PAGE WIDTH AND HEIGHT FOR USE
     var wWidth = $( window ).width();
@@ -224,9 +228,5 @@ jQuery(document).ready(function($){
 
     //CONTENT BOX DIMENSION FIX AND NORMALIZE HEIGHT
     fixTwoColLayout(wWidth);
-    $(window).resize(function(){fixTwoColLayout(wWidth);});
-
-    
-
- 
+    $(window).resize(function(){fixTwoColLayout(wWidth);}); 
 });
